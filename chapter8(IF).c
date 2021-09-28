59e2f908f088f2203e874565cb6dfb1bb7c83b3c
jeon <yjin1185@gmail.com>
2021-09-28 21:12:39 +0900

    2021.09.08 문제 08-01

--- chapter8(IF).c
+++ chapter8(IF).c
@@ -2,7 +2,7 @@
 
 int main (void){
 
-    /*for(int num = 1; num < 100; num++) {
+    /*for(int num = 1; num < 100; num++) { 08-01-01 문제
         if (num % 7 == 0 && num % 9 ==0){
             printf("%d 는 7과 9의 배수입니다. \n" , num);
         }
@@ -16,9 +16,9 @@ int main (void){
         }
 
 
-    }*/
+    }
 
-    int num1, num2, total;
+    int num1, num2, total; 08-01-02 문제
 
     printf("숫자를 입력하세요 : ");
     scanf("%d", &num1);
@@ -39,6 +39,35 @@ int main (void){
         printf("%d - %d = %d ", num2,num1,total);
     }
 
+    int Score;  08-01-03 문제
+
+    printf("점수를 입력하시오");
+    scanf("%d" , &Score);
+
+    if (Score >= 90){
+        printf("학점이 A 입니다.");
+    }
+
+    else if (Score < 90 && Score >=80)
+    {
+        printf("학점이 B 입니다.")
+    }
+
+    else if (Score < 80 && Score >=70)
+    {
+        printf("학점이 C 입니다.")
+    }
+    
+    else if (Score < 70 && Score >=50)
+    {
+        printf("학점이 D 입니다.")
+    }
+
+    else{
+
+        printf(" 학점이 F 입니다.");
+    }
+
     return 0;
 
-}
+}*/
