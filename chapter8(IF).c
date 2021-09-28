7b46bf3c58501c19f2e5662d936a095ddd450369
jeon <yjin1185@gmail.com>
2021-09-28 20:55:52 +0900

    파일변경

--- chapter8(IF).c
+++ chapter8(IF).c
@@ -3,15 +3,15 @@
 int main (void){
 
     for(int num = 1; num < 100; num++) {
-        if (num / 7 == 0 && num / 9 ==0){
+        if (num % 7 == 0 && num % 9 ==0){
             printf("%d 는 7과 9의 배수입니다. \n" , num);
         }
 
-        else if (num / 7 == 0) {
+        else if (num % 7 == 0) {
             printf("%d 는 7의 배수입니다. \n" , num);
         }
 
-        else if (num / 9 == 0) {
+        else if (num % 9 == 0) {
             printf("%d 는 9의 배수입니다. \n" , num);
         }
 
