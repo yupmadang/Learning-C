943f52ff9a019b946b1138520bd94795bfced446
jeon <yjin1185@gmail.com>
2021-09-28 20:43:38 +0900

    문제 8-1

---
+++ chapter8(IF).c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main (void){
+
+    for(int num = 1; num < 100; num++) {
+        if (num / 7 == 0 && num / 9 ==0){
+            printf("%d 는 7과 9의 배수입니다. \n" , num);
+        }
+
+        else if (num / 7 == 0) {
+            printf("%d 는 7의 배수입니다. \n" , num);
+        }
+
+        else if (num / 9 == 0) {
+            printf("%d 는 9의 배수입니다. \n" , num);
+        }
+
+
+    }
+
+    return 0;
+
+}
