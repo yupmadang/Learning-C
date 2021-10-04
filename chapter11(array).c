2ea93ed4d1366cd6814dfabff10b650cbbb6570f
jeon <yjin1185@gmail.com>
2021-10-04 13:25:23 +0900

    chapter11(array)2021.10.04  문제 1-1,1-2

---
+++ chapter11(array).c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+
+/*int main(void) { 2021.10.04 11-1-1 문제풀이 : 배열입력 받아 합, 최댓값, 최솟값 구하기
+
+    int arr[5];
+    int Max, Min, Sum;
+
+    for (int i = 0; i < 5; i++) {
+        printf("숫자를 입력하시오 : ");
+        scanf("%d", &arr[i]);
+    }
+
+    Max = Min = Sum = arr[0];
+    for (int i = 1; i < 5; i++) {
+        Sum += arr[i];
+        if (Max < arr[i])
+            Max = arr[i];
+        if (Min > arr[i])
+            Min = arr[i];
+
+    }
+
+    printf("합: %d \n", Sum);
+    printf("최댓값: %d \n", Max);
+    printf("최솟값: %d \n", Min);
+
+
+    return 0;
+}*/
+
+/*int main(void) { 2021.10.04 11-1-2 문제풀이 : 문자 입력 및 출력
+
+    char arr[9];
+    for (int i = 0; i < 9; i++) {
+        scanf("%c", &arr[i]);
+
+    }
+
+    for (int i = 0; i < 9; i++) {
+        printf("결과 : %c \n", arr[i]);
+
+    }
+
+
+    return 0;
+}*/
