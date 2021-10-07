d4f08549d8bef79c38d83178c8e4852a91a6bfae
Yongjin Jeon <yjin1185@gmail.com>
2021-10-07 16:11:27 +0900

    chapter13 예제 3번

--- chapter13.c
+++ chapter13.c
@@ -27,3 +27,21 @@ int main (void){
     return 0;
 }*/
 
+int main (void){
+
+    int arr[3] = {11, 22, 33};
+    int * ptr = arr;
+    printf("%d %d %d \n", *ptr, *(ptr+1), *(ptr+2));
+
+    printf("%d ", *ptr); ptr++;
+    printf("%d ", *ptr); ptr++;
+    printf("%d ", *ptr); ptr--;
+    printf("%d ", *ptr); ptr--;
+    printf("%d ", *ptr); printf("\n");
+
+
+    return 0;
+}
+
+
+
