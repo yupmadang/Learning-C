4d6cec03a8bbe5d2237fae92e8dc61ff09877036
Yongjin Jeon <yjin1185@gmail.com>
2021-10-08 15:11:49 +0900

    chapter14 예제

---
+++ chapter14.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*void ShowArrayElem(int *param, int len){ 배열의 주소 값을 전달받는 함수 + 배열의 길이도 추가
+    for (int i = 0; i < len ; i++){
+        printf("%d ", param[i]);
+    }
+    printf("\n");
+}
+
+int main (void){
+
+    int arr1[3] = {1, 2, 3};
+    int arr2[5] = {4, 5, 6, 7, 8};
+    ShowArrayElem(arr1, sizeof(arr1) / sizeof(int)); arr1의 주소 값을 복사하여 함수에 전달
+    ShowArrayElem(arr2, sizeof(arr2) / sizeof(int)); arr2의 주소 값을 복사하여 함수에 전달
+
+
+    return 0;
+}*/
