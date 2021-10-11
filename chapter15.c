f6860e4fa4ca9a6e66f5f0ae286c35464ef1b3a0
Yongjin Jeon <yjin1185@gmail.com>
2021-10-11 23:31:59 +0900

    chapter15 문제 1번 2021.10. 11

---
+++ chapter15.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/*void CalcArr(int arr[]){ 문제 1번 : 배열을 이용한 홀수, 짝수 출력문제
+
+    printf("홀수 출력 : ");
+    for(int i = 0; i < 10 ; i++ ){
+        if(arr[i] % 2 != 0){
+            printf("%d ", arr[i]);
+        }
+    }
+}
+
+void CalcArr2(int arr[]){
+
+    printf("짝수 출력 : ");
+    for (int i =0 ; i < 10 ; i++){
+        if(arr[i] % 2 == 0 ){
+            printf("%d ", arr[i]);
+        }
+    }
+}
+
+
+
+int main (void){
+
+    int arr[10];
+    for (int i = 0; i < 10; i++){
+        printf("배열을 입력하시오 : ");
+        scanf("%d", &arr[i]);
+
+    }
+
+    CalcArr(arr);
+    CalcArr2(arr);
+
+
+    return 0;
+}*/
