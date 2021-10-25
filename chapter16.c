d4322703f06f21f1b85982a325350160087739be
Yongjin Jeon <yjin1185@gmail.com>
2021-10-25 17:09:43 +0900

    chapter16 연습문제 2번

--- chapter16.c
+++ chapter16.c
@@ -145,4 +145,28 @@ int main (void){ 연습문제 1번 2021.10.25 2차원 배열을 이용한 구구단 출력
     }
 
     return 0;
+}
+
+int main (void){ 연습문제 2번 2021.10.25 arr1을 이용한 arr2 초기화
+
+    int arr1[2][4] = {1,2,3,4,5,6,7,8};
+    int arr2[4][2];
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 2; j++){
+            arr2[i][j] = arr1[j][i];
+        }
+    }
+
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < 2; j++){
+            printf("%2d",arr2[i][j]);
+        }
+        printf("\n");
+    }
+
+
+ 
+
+    return 0;
 }*/
