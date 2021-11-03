1ce551af1b00c648a122432daaf49c2b8ecebb9f
Yongjin Jeon <yjin1185@gmail.com>
2021-11-03 19:17:03 +0900

    chapter20 도전프로그래밍 2,3,4번

--- chapter20.c
+++ chapter20.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 /*void reviseArr(int (*arr) [4], int len){ 도전 프로그래밍 3 2021.11 03 문제 1번, 90도 돌리는 2차원 배열
 
@@ -41,3 +43,88 @@ int main (void){
     }
     return 0;
 }*/
+
+/*void arrs (int (*arr) [100], int len){ 도전 프로그래밍 3 문제 2번 2021.11.03
+
+    int num = 0;
+    int k = 0;
+    while (k < len - 2) {
+
+		
+		for (int i = k; i < len - k; i++) {
+			arr[k][i] = ++num;
+		}
+
+		
+		for (int i = k + 1; i < len - k; i++) {
+			arr[i][(len - k) - 1] = ++num;
+		}
+
+		
+		for (int i = (len - k) - 2; i >= k; i--) {
+			arr[(len - k) - 1][i] = ++num;
+		}
+
+		
+		for (int i = (len - k) - 2; i > k; i--) {
+			arr[i][k] = ++num;
+		}
+
+		k++;
+	}
+
+}
+
+int main (void){
+
+    int num;
+    int arr[100][100];
+
+    printf("수를 입력하세요 : ");
+    scanf("%d \n", &num);
+
+    arrs(arr,num);
+    
+    for(int i = 0; i < num; i++){
+        for(int j = 0; j < num; j++){
+            printf("%5d", arr[i][j]);
+        }
+        printf("\n");
+    }
+}*/
+
+/*int main (void){ 난수 생성 후 이를 99이하로 출력 문제3 2021. 11. 03 문제 3번
+
+    int i;
+
+    printf("난수의 범위 : 0부터 %d까지 \n", RAND_MAX);
+    for(int i = 0; i < 5; i++){
+        printf("난수 출력: %d \n", rand());
+    }
+
+    
+    printf("난수의 범위 : 0부터 99까지 \n", RAND_MAX);
+    for(int i = 0; i < 5; i++){
+        printf("난수 출력: %d \n", rand()%100);
+    }
+
+    return 0;
+}*/
+
+
+
+/*int main (void){ 현재시간을 인자로 전달받아 주사위의 값을 출력하는 문제 2021. 11. 03 문제 4번
+
+    srand((int)time(NULL));
+
+    int dice1;
+    int dice2;
+    int method = rand() % 6 + 1;
+    int method2 = rand() % 6 + 1;
+
+    printf("첫 주사위의 값 : %d \n", method);
+    printf("두 번째 주사위 값 : %d \n", method2);
+
+
+    return 0; */
+
