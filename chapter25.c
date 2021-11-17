d042f4a26288e0036f0cff1f798954d930328365
Yongjin Jeon <yjin1185@gmail.com>
2021-11-17 20:29:10 +0900

    chapter25 malloc 예제

---
+++ chapter25.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+int main (void)
+{
+    int * ptr1 = (int *)malloc(sizeof(int));     malloc 함수를 이용하여 힙메모리에 int 크기만큼의 메모리를 할당한다. - malloc 함수는 void 포인터로 사용시 포인터 형을 선언하여야 한다.
+    int * ptr2 = (int *)malloc(sizeof(int)*7);   malloc 함수는 void 포인터로 사용시 포인터 형을 선언하여야 한다.
+
+    *ptr1 = 20; 
+    for( int i = 0; i < 7; i++)
+    {
+        ptr2[i] = i+1;
+    }
+
+    printf("%d \n", *ptr1);
+    for(int i = 0; i < 7; i++)
+    {
+        printf("%d \n", ptr2[i]);
+    }
+
+    free(ptr1);     free함수를 이용하여 힙메모리에 할당된 메모리를 소멸하고 있다.
+    free(ptr2);
+    return 0;
+}
+*/
